41e6e293c19271cce1fce27da99b0c1860d1602d
Vincente <vciancio@socaldevs.com>
2016-05-21 11:37:01 -0700

    fixed more stuff

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Adresses (
+CREATE TABLE Addresses(
 	id 			CHAR(15) NOT NULL PRIMARY KEY,
 	street		VARCHAR(15) NOT NULL,
 	city 		VARCHAR(15) NOT NULL,
@@ -7,42 +7,47 @@ CREATE TABLE Adresses (
 
 CREATE TABLE Branches (
 	id 			CHAR(15) NOT NULL PRIMARY KEY,
-	addressId	CHAR(15) FOREIGN KEY REFERENCES Adresses(id),
-	phone		NUMBER(10) NOT NULL
+	addressId	CHAR(15) ,
+	phone		NUMBER(10) NOT NULL,
+	FOREIGN KEY (addressId) REFERENCES Addresses(id)
 );
 
 CREATE TABLE Employees (
 	id 			CHAR(15) NOT NULL PRIMARY KEY,
-	branchId	CHAR(15) FOREIGN KEY REFERENCES Branches(id),
+	branchId	CHAR(15),
 	name		VARCHAR(15) NOT NULL,
 	phone		NUMBER(10),
 	startDate	DATE NOT NULL,
-	job			VARCHAR(15) NOT NULL
+	job			VARCHAR(15) NOT NULL,
+	FOREIGN KEY (branchId) REFERENCES Branches(id)
 );
-
 
 CREATE TABLE Property_Owners (
 	id 			CHAR(15) NOT NULL PRIMARY KEY,
-	addressId	CHAR(15) FOREIGN KEY REFERENCES Adresses(id),
+	addressId	CHAR(15),
 	name		VARCHAR(15),
 	phone		NUMBER(10),
-	fees		NUMERIC(5,2)
+	fees		NUMERIC(5,2),
+	FOREIGN KEY (addressId) REFERENCES Addresses(id)
 );
 
 CREATE TABLE Rental_Properties (
 	propertyId	 CHAR(15) NOT NULL PRIMARY KEY,
-	ownerId		 CHAR(15) FOREIGN KEY REFERENCES Property_Owners(id),
-	address 	 CHAR(15) FOREIGN KEY REFERENCES Adresses(id),
+	ownerId		 CHAR(15),
+	addressId 	 CHAR(15),
 	numRooms	 NUMBER(2) NOT NULL,
 	rent 		 NUMERIC(7,2) NOT NULL,
-	status 		 BOOLEAN NOT NULL,
+	status 		 INTEGER NOT NULL,
 	availDate	 DATE NOT NULL,
-	supervisorId CHAR(15) FOREIGN KEY REFERENCES Employees(id)
+	supervisorId CHAR(15),
+	FOREIGN KEY (ownerId) REFERENCES Property_Owners(id),
+	FOREIGN KEY (addressId) REFERENCES Addresses(id),
+	FOREIGN KEY (supervisorId) REFERENCES Employees(id)
 );
 
 CREATE TABLE LeaseAgreement (
 	leaseId		 CHAR(15) NOT NULL PRIMARY KEY,
-	propertyId 	 CHAR(15) FOREIGN KEY Rental_Properties(propertyId),
+	propertyId 	 CHAR(15) ,
 	homePhone	 NUMBER(10),
 	workPhone	 NUMBER(10),
 	contactName	 VARCHAR(15),
@@ -50,5 +55,6 @@ CREATE TABLE LeaseAgreement (
 	startDate	 DATE,
 	endDate		 DATE,
 	totalRent	 NUMERIC(8, 2),
-	deposit		 NUMERIC(4, 2)
+	deposit		 NUMERIC(4, 2),
+	FOREIGN KEY (propertyId) REFERENCES Rental_Properties(propertyId)
 );
